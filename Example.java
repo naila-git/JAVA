d312590cc66043ad94b65b382aaf8a61ddb8889a
Naila Imran <inaila019@gmail.com>
2026-05-02 07:17:08 -0500

    Java Practice

---
+++ Example.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+public class Example {
+    public static void main(String[] args){
+
+        // need one scanner object to make Java interactive 
+        Scanner scanner = new Scanner(System.in);
+        //System.out.println("Write a Message");
+
+        //String message = scanner.nextLine();
+
+        //System.out.println(message);
+
+        //String start = "My name is ";
+        //String end = ", James Bond";
+
+        //System.out.println(start + "Bond" + end);
+
+
+
+        //String message2 = scanner.nextLine();
+        //System.out.println("You wrote: " + message2);
+        //System.out.println(message2);
+        //System.out.println(message2);
+
+        //Integer.valueof() takes the string and converts it into and integer (Wrapper object for int)
+        //int num = Integer.valueOf(scanner.nextLine());
+        //System.out.println(Math.sqrt(num));
+
+        // write a program that take two #s from user and sums their square root
+
+        int num1 = scanner.nextLine()
+
+
+
+
+    }
+}
